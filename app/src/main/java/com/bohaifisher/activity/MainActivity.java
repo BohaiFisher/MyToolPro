3dd560637761441ced6cf85e388184774ff515bb
BohaiFisher <1146603301@qq.com>
2018-04-27 16:09:57 +0800

    fisher: add something about broadcast receiver.

--- app/src/main/java/com/bohaifisher/activity/MainActivity.java
+++ app/src/main/java/com/bohaifisher/activity/MainActivity.java
@@ -1,17 +1,41 @@
 package com.bohaifisher.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.bohaifisher.base.BaseActivity;
+import com.bohaifisher.broadreceiver.BroadcastTestActivity;
+import com.bohaifisher.mytoolpro.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.btn_broadcast_test)
+    Button btnBroadcastTest;
+    @BindView(R.id.btn_dialog_test)
+    Button btnDialogTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Toast.makeText(this, "you should keep quiet, think less, do more", Toast.LENGTH_SHORT).show();
 
+    }
+
+    @OnClick({R.id.btn_broadcast_test, R.id.btn_dialog_test})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_broadcast_test:
+                startActivity(BroadcastTestActivity.class);
+                break;
+            case R.id.btn_dialog_test:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/bohaifisher/broadreceiver/BroadcastTestActivity.java
@@ -0,0 +1,16 @@
+package com.bohaifisher.broadreceiver;
+
+import android.os.Bundle;
+
+import com.bohaifisher.base.BaseActivity;
+import com.bohaifisher.mytoolpro.R;
+
+
+public class BroadcastTestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_test);
+    }
+}
